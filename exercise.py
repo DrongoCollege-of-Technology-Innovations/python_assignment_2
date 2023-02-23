2e3d38633a5760050e7f3e51d080887bc37765eb
kelvinokumu <oumakelvinokumu.com>
2023-02-23 16:27:43 +0300

    python assignmenr 2

---
+++ exercise.py
@@ -0,0 +1,11 @@
+# complete the function below and return the reverse of the given number
+
+def reverse():
+    num = 123456789
+    reverse_number = 0
+    # add your code here
+
+
+    return reverse_number
+
+reverse()
---
+++ test_exercise.py
@@ -0,0 +1,10 @@
+from exercise import reverse
+
+
+def test_reverse():
+    assert reverse() == 987654321, "Error"
+
+
+if __name__ == '__main__':
+    test_reverse()
+    print("Test passed")
